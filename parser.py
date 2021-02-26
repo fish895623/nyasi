22c688a69d213c89b536c2a9da2aa2df4756e29b
fish895623 <dan990429@gmail.com>
2021-02-26 09:29:43 +0000

    bs4 select

--- parser.py
+++ parser.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-req = requests.get('https://nyaa.si')
+req = requests.get("https://nyaa.si")
 html = req.text
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, "html.parser")
 
+# NOTE - Get title from Site
+# body > div > div.table-responsive > table > tbody > tr:nth-child(1) > td:nth-child(2) > a
 my_titles = soup.select(
-    'body > div > div.table-responsive > table > tbody > tr > td:nth-child(2) > a'
+    "body > div > div.table-responsive > table > tbody > tr > td > a"
 )
+for i in my_titles:
+    i.text.replace(" ", "_")
+    print(i.text)
 
-# body > div > div.table-responsive > table > tbody > tr:nth-child(4) > td:nth-child(2) > a
-
-mg = soup.select(
-    'body > div > div.table-responsive > table > tbody > tr > td:nth-child(3) > a:nth-child(2)')
-# body > div > div.table-responsive > table > tbody > tr:nth-child(4) > td:nth-child(3) > a:nth-child(2)
-
-print(mg[0])
+# body > div > div.table-responsive > table > tbody > tr:nth-child(1) > td:nth-child(2) > a
+# body > div > div.table-responsive > table > tbody > tr:nth-child(1) > td:nth-child(3) > a:nth-child(2)
